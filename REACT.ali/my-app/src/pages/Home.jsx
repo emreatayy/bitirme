b05a783bf695e0267a3683fa57a1c569b971f7a5
Ali Yusuf Erdoğan <87943506+alierdo21@users.noreply.github.com>
2023-04-19 15:06:06 +0300

    bitirmenavbar
    tten

---
+++ REACT.ali/my-app/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+
+
+import React, { Fragment } from "react";
+import Header from "../components/Header/Header";
+
+const Home=()=>{
+    return (
+         <Fragment>
+        <Header />
+    </Fragment>
+    );
+};
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
