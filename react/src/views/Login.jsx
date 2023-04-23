ab48544d9b8c6c973f349b023295f4d7a5908fee
Yunus EMRE Atay <yns.emreatay@gmail.com>
2023-04-23 13:13:00 +0300

    updt

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -6,24 +6,38 @@ export default function Login(){
         ev.preventDefault();
     }
 
+    const down = event => {
+        const label = document.getElementById("label" + event.target.id);
+        console.log(event.target.id)
+        if (event.target.value.length > 0){
+            label.style.top= "32px";
+            label.style.left= "0";
+            label.style.color= "#03e9f4";
+            label.style.fontSize= "12px";
+            label.style.color = "#329E32";
+        }
+    };
+
     return(
         <div className="login-box">
             <h2>Login</h2>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="user-box">
-                    <input type="text" name="" required="" />
-                    <label className="label">Username</label>
+                    <input type="text" name="" required="" id="1" onChange={down}/>
+                    <label className="label" id="label1">Username</label>
                 </div>
                 <div className="user-box">
-                    <input type="password" name="" required="" />
-                    <label className="label">Password</label>
+                    <input type="password" name="" required="" id="2" onChange={down}/>
+                    <label className="label" id="label2">Password</label>
                 </div>
                 <a href="#">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Submit
+                    <button>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Submit
+                    </button>
                 </a>
                 <Link to="/Signup">Do not have an account?</Link>
             </form>
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,39 +1,73 @@
 import {Link} from "react-router-dom";
+import {useRef} from "react";
 
 export default function Signup(){
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
     const onSubmit = (ev) => {
         ev.preventDefault();
+        const payload = {
+            first_name: firstNameRef.current.value,
+            last_name: lastNameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        console.log(payload)
     }
+
+    const down = event => {
+        const label = document.getElementById("label" + event.target.id);
+        console.log(event.target.id)
+        if (event.target.value.length > 0){
+            label.style.top= "32px";
+            label.style.left= "0";
+            label.style.color= "#03e9f4";
+            label.style.fontSize= "12px";
+            label.style.color = "#329E32";
+        }
+    };
 
     return(
         <div className="login-box">
-            <h2>Signup</h2>
-            <form>
-                <div className="user-box">
-                    <input type="text" name="" required="" />
-                    <label className="label">Full Name</label>
-                </div>
-                <div className="user-box">
-                    <input type="text" name="" required="" />
-                    <label className="label">Email Adress</label>
-                </div>
-                <div className="user-box">
-                    <input type="password" name="" required="" />
-                    <label className="label">Password</label>
-                </div>
-                <div className="user-box">
-                    <input type="password" name="" required="" />
-                    <label className="label">Password Confirm</label>
-                </div>
-                <a href="#">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Submit
-                </a>
-                <Link to="/Login">Already have an account?</Link>
-            </form>
-        </div>
+        <h2>Signup</h2>
+        <form onSubmit={onSubmit}>
+            <div className="user-box">
+                <input id="1"  ref={firstNameRef} type="text" onChange={down} required=""/>
+                <label id="label1">First Name</label>
+            </div>
+            <div className="user-box" >
+                <input id="2"  ref={lastNameRef} type="text" name=""  onChange={down} required=""/>
+                <label id="label2">Last Name</label>
+            </div>
+            <div className="user-box">
+                <input id="3"  ref={emailRef} type="email" name=""  onChange={down} required="" />
+                <label id="label3">E-mail</label>
+            </div>
+            <div className="user-box">
+                <input id="4"  ref={passwordRef} type="password" name=""  onChange={down} required="" />
+                <label id="label4">Password</label>
+            </div>
+            <div className="user-box">
+                <input id="5"  ref={passwordConfirmationRef} type="password" name=""  onChange={down} required=""/>
+                <label id="label5">Password Confirmation</label>
+
+            </div>
+            <a href="#">
+                <button>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+                </button>
+            </a>
+            <Link to="/Login">Already have an account?</Link>
+        </form>
+    </div>
     )
 }
